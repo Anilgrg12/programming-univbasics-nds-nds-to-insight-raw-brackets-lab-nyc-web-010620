bef13c8a9ddd4541c82140b6efd7a2be6c227835
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 04:38:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,8 @@ require 'pp'
 def directors_totals(nds)
   
   grand_total = 0 
-  
+  index = 0 
+  while index < 
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
