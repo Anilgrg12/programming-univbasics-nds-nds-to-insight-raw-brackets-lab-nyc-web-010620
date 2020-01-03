7e1259eeb44b3c77e5b621499b6f25c0da9ac38b
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 04:42:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,9 +4,18 @@ require 'pp'
 
 def directors_totals(nds)
   
-  grand_total = 0 
+  ret_hash= {}
   index = 0 
-  while index < 
+  while nds[index] do
+    sum = 0 
+    num_movies=0 
+    while nds[index][:movies][num_movies] do
+      sum += nds[index][:movies][num_movies][:worldwide_gross]
+      num_movies += 1 
+    end
+    ret_hash[nds[index][:name]]= sum    
+    index += 1
+  end
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
